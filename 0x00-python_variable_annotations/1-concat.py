7763c899c003d3952c00bbceb955d19a4b132033
AnasMouak <anasmouak1111@gmail.com>
2024-08-01 11:39:01 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    This function takes two string arguments and returns their concatenation.
+
+    Parameters:
+    str1 (str): The first string to be concatenated.
+    str2 (str): The second string to be concatenated.
+
+    Returns:
+    str: The concatenation of the two strings.
+    """
+    return str1 + str2
