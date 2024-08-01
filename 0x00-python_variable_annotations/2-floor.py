08a625eecc6141747418b12e917c760f8a224761
AnasMouak <anasmouak1111@gmail.com>
2024-08-01 11:51:44 +0000

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,4 +4,16 @@ import math
 
 
 def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    This function takes a float argument and returns the largest integer
+    less than or equal to the given float.
+
+    Parameters:
+    n (float): The float whose floor value is to be calculated.
+
+    Returns:
+    int: The floor value of the float.
+    """
     return math.floor(n)
