ce54a6f2fe71ff6d73165f183a68a2a2bb0e4883
AnasMouak <anasmouak1111@gmail.com>
2024-08-06 17:10:57 +0000

    Update

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Asynchronous function that collects values from async_generator into a list
 
